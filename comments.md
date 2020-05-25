e3d8682beec49ec2d0174b28c5bcff7bcdbd57b8
Jitgitbit <rowowolf@gmail.com>
2020-05-25 18:18:11 +0200

    google maps JS sdk

--- comments.md
+++ comments.md
@@ -3,3 +3,5 @@ npx create-react-app react-mern-exercise-two-frontend
 npm install --save react-router-dom@5 --save-exact
 
 npm install --save react-transition-group
+
+google maps JS sdk
---
+++ src/shared/components/UIElements/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Map.css'
+
+export default function Map(props) {
+  return (
+    <div className={`map ${props.className}`} style={props.style}>
+      
+    </div>
+  )
+}
