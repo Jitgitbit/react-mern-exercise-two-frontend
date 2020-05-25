9f3ba8eaccc6f3066f7e96c7a8971d6c59be0172
Jitgitbit <rowowolf@gmail.com>
2020-05-25 17:43:54 +0200

    !!! Using 2 components in 1 file !!!

--- src/shared/components/UIElements/Modal.jsx
+++ src/shared/components/UIElements/Modal.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
 import ReactDOM from "react-dom";
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition } from "react-transition-group";
 
-import './Modal.css'
+import "./Modal.css";
 
-import Backdrop from './Backdrop'
+import Backdrop from "./Backdrop";
 
-
-const ModalOverlay = props => {
+const ModalOverlay = (props) => {
   const content = (
     <div className={`modal ${props.className}`} style={props.style}>
       <header className={`modal__header ${props.headerClass}`}>
         <h2>{props.header}</h2>
       </header>
-      <form onSubmit={props.onSubmit ? props.onSubmit : event => event.preventDefault}>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault
+        }
+      >
         <div className={`modal__content ${props.contentClass}`}>
           {props.children}
         </div>
@@ -23,14 +26,22 @@ const ModalOverlay = props => {
       </form>
     </div>
   );
-  return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
-}
+  return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
+};
 
 export default function Modal(props) {
   return (
     <React.Fragment>
-      {props.show && <Backdrop onClick={props.onCancel}/>}
-      <CSSTransition in={props.show} mountOnEnter unmountOnExit timeout={300} classNames="modal"></CSSTransition>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={300}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
     </React.Fragment>
-  )
+  );
 }
