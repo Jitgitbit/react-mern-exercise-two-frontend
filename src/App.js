135260b2bee296194b69297e8832ecd5f3f85e55
Jitgitbit <rowowolf@gmail.com>
2020-05-25 11:03:23 +0200

    space created between navigation and main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <Router>
-      <MainNavigation/>
-      <Switch>
-        <Route path="/" exact>
-          <Users/>
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace/>
-        </Route>
-        <Redirect to="/"/> 
-      </Switch>             
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
