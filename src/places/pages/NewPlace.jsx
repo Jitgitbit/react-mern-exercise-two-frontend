b242d1205535972dec9614e3334599d3392732b9
Jitgitbit <rowowolf@gmail.com>
2020-05-27 14:28:47 +0200

    ! avoiding INF LOOP with useCallback !!!

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import "./NewPlace.css";
 
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/util/validators";
 
 export default function NewPlace() {
 
-  const titleInputHandler = (id, value, isValid) => {};      //------> DANGER ! INFINITE LOOP !
+  // const titleInputHandler = (id, value, isValid) => {};      //------> DANGER ! INFINITE LOOP !
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);                             //--> no more infinite loop !
 
   return (
     <form className="place-form">
       <Input
         element="input"
+        id="title"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid Title."
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        element="textarea"
+        id="description"
+        label="Description"
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
         onInput={titleInputHandler}
       />
     </form>
