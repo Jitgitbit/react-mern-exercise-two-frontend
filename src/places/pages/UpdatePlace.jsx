2c12b4e0a1adde48404fa708eed7450ca7532300
Jitgitbit <rowowolf@gmail.com>
2020-05-27 16:27:04 +0200

    use the correct identifier !

--- src/places/pages/UpdatePlace.jsx
+++ src/places/pages/UpdatePlace.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function UpdatePlace() {
+  const placeId = useParams().placeId;
+  
   return (
     <div>
       Update Place
