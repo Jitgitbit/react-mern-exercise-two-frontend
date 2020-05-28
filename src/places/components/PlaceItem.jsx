e5046bc24691de262348a1fd2089f155efae44f0
Jitgitbit <rowowolf@gmail.com>
2020-05-28 10:57:35 +0200

    Showing a Deletion Warning

--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -10,9 +10,21 @@ import Map from "../../shared/components/UIElements/Map";
 export default function PlaceItem(props) {
 
   const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   const openMapHandler = () => setShowMap(true);
   const closeMapHandler = () => setShowMap(false);
+
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true)
+  }
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false)
+  }
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false);
+    console.log('DELETING...')
+  }
 
   return (
     <React.Fragment>
@@ -28,6 +40,14 @@ export default function PlaceItem(props) {
           <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
+      <Modal onCancel={cancelDeleteHandler} show={showConfirmModal} header="Are you sure?" footerClass="place-item__modal-actions" footer={
+        <React.Fragment>
+          <CustomButton inverse onClick={cancelDeleteHandler}>CANCEL</CustomButton>
+          <CustomButton danger onClick={confirmDeleteHandler}>DELETE</CustomButton>
+        </React.Fragment>
+      }>
+        <p>Do you want to proceed and delete this place?</p>
+      </Modal>
       <li className="place-item">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -41,7 +61,7 @@ export default function PlaceItem(props) {
           <div className="place-item__actions">
             <CustomButton inverse onClick={openMapHandler}>VIEW ON MAP</CustomButton>
             <CustomButton to={`/places/${props.id}`}>EDIT</CustomButton>
-            <CustomButton danger>DELETE</CustomButton>
+            <CustomButton danger onClick={showDeleteWarningHandler}>DELETE</CustomButton>
           </div>
         </Card>
       </li>
