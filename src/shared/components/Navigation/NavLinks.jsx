7118c3e48f00b11df55a155e249962a5daf6e016
Jitgitbit <rowowolf@gmail.com>
2020-05-28 14:45:45 +0200

    logout added

--- src/shared/components/Navigation/NavLinks.jsx
+++ src/shared/components/Navigation/NavLinks.jsx
@@ -30,6 +30,11 @@ export default function NavLinks(props) {
           <NavLink to="/auth">AUTHENTICATE</NavLink>
         </li>
       )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
     </ul>
   )
 }
