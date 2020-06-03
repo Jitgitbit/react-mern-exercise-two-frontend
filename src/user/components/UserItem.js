b48593ce04dd32f65ad5cf729d9ee5560083349d
Jitgitbit <rowowolf@gmail.com>
2020-06-03 06:51:06 +0200

    User clean up done

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,12 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./UserItem.css";
+import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
+import './UserItem.css';
 
-import Avatar from "../../shared/components/UIElements/Avatar";
-import Card from "../../shared/components/UIElements/Card";
-
-export default function UserItem(props) {
+const UserItem = props => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
@@ -17,11 +16,13 @@ export default function UserItem(props) {
           <div className="user-item__info">
             <h2>{props.name}</h2>
             <h3>
-              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+              {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
             </h3>
           </div>
         </Link>
       </Card>
     </li>
   );
-}
+};
+
+export default UserItem;
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,59 +1,69 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
 
-import './Auth.css'
-
-import { useFormHook } from '../../shared/hooks/form-hook'
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import Button from '../../shared/components/FormElements/Button';
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 import { AuthContext } from '../../shared/context/auth-context';
+import './Auth.css';
 
-
-export default function Auth() {
-
+const Auth = () => {
   const auth = useContext(AuthContext);
-
   const [isLoginMode, setIsLoginMode] = useState(true);
 
-  const [formState, inputHandler, setFormData] = useFormHook({
-    email: {
-      value: '',
-      isValid: false
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      email: {
+        value: '',
+        isValid: false
+      },
+      password: {
+        value: '',
+        isValid: false
+      }
     },
-    password: {
-      value: '',
-      isValid: false
+    false
+  );
+
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: '',
+            isValid: false
+          }
+        },
+        false
+      );
     }
-  }, false);
+    setIsLoginMode(prevMode => !prevMode);
+  };
 
   const authSubmitHandler = event => {
     event.preventDefault();
-    console.log(`--->> auth inputs say what?`,formState.inputs);
+    console.log(formState.inputs);
     auth.login();
-  }
-  const switchModeHandler = () => {
-    if(!isLoginMode){
-      setFormData({
-        ...formState.inputs,
-        name: undefined
-      }, formState.inputs.email.isValid && formState.inputs.password.isValid);
-    }else{
-      setFormData({
-        ...formState.inputs,
-        name: {
-          value: '',
-          isValid: false
-        }
-      }, false);
-    }
-    setIsLoginMode(prevMode => !prevMode);
-  }
+  };
 
   return (
     <Card className="authentication">
       <h2>Login Required</h2>
-      <hr/>
+      <hr />
       <form onSubmit={authSubmitHandler}>
         {!isLoginMode && (
           <Input
@@ -71,7 +81,7 @@ export default function Auth() {
           id="email"
           type="email"
           label="E-Mail"
-          validators={[VALIDATOR_EMAIL]}
+          validators={[VALIDATOR_EMAIL()]}
           errorText="Please enter a valid email address."
           onInput={inputHandler}
         />
@@ -92,5 +102,7 @@ export default function Auth() {
         SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
       </Button>
     </Card>
-  )
-}
+  );
+};
+
+export default Auth;
