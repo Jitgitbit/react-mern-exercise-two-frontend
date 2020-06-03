cdaaf4f2cb616f76ee53e31d6d84c9804701b0aa
Jitgitbit <rowowolf@gmail.com>
2020-06-03 18:44:47 +0200

    Deleting Places works

--- src/places/components/PlaceList.jsx
+++ src/places/components/PlaceList.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
 import Button from '../../shared/components/FormElements/Button';
-
 import './PlaceList.css';
-
 
 const PlaceList = props => {
   if (props.items.length === 0) {
@@ -31,6 +29,7 @@ const PlaceList = props => {
           address={place.address}
           creatorId={place.creator}
           coordinates={place.location}
+          onDelete={props.onDeletePlace}
         />
       ))}
     </ul>
