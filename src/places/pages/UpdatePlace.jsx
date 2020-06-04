dc0171866ea8bcce138c0ce2eedd329ae6d934e4
Jitgitbit <rowowolf@gmail.com>
2020-06-04 20:28:08 +0200

    Using Tokens to Update and Delete Places

--- src/places/pages/UpdatePlace.jsx
+++ src/places/pages/UpdatePlace.jsx
@@ -72,7 +72,6 @@ const UpdatePlace = () => {
           },
           true
         );
-
       } catch (err) {}
     };
     fetchPlace();
@@ -89,7 +88,8 @@ const UpdatePlace = () => {
           description: formState.inputs.description.value
         }),
         {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + auth.token
         }
       );
       history.push('/' + auth.userId + '/places');
